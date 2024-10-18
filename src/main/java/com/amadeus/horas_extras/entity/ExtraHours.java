79aeeb321814838a5148d381f370adda741cf087
Andrés Felipe Toro González <pipetoro19@hotmail.com>
2024-10-17 20:24:06 -0500

    add new class extrahours

---
+++ src/main/java/com/amadeus/horas_extras/entity/ExtraHours.java
@@ -0,0 +1,46 @@
+package com.amadeus.horas_extras.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "horas")
+
+public class ExtraHours {
+
+    private int id;
+   @Column(name = "horainicio")
+    private LocalDateTime startHours ;
+   @Column(name = "horafin")
+    private LocalDateTime endHours ;
+   @Column(name = "horadiurna")
+    private Double dayTimeHours;
+   @Column(name = "horanocturna")
+    private Double nightTimeHours;
+   @Column(name = "hora")
+    private Double dayHolidayHours;
+   @Column(name = "")
+    private Double nightHolidayHours;
+   @Column(name = "")
+    private BigDecimal valuedayhours;
+   @Column(name = "")
+    private BigDecimal valuenighthours;
+   @Column(name = "")
+    private BigDecimal valueDayHollidayHours;
+   @Column(name = "")
+    private BigDecimal valueNightHolidayHours;
+   @Column(name = "")
+    private BigDecimal valueday;
+
+
+}

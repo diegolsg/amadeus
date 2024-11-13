499afbe8292eba80230224de0a2a05308f990df6
Andrés Felipe Toro González <pipetoro19@hotmail.com>
2024-11-12 21:59:19 -0500

    Fix springboot security and Pom

--- src/test/java/com/amadeus/horas_extras/HorasExtrasApplicationTests.java
+++ src/test/java/com/amadeus/horas_extras/HorasExtrasApplicationTests.java
@@ -1,13 +1,13 @@
 package com.amadeus.horas_extras;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class HorasExtrasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test case logic
+    }
 }
